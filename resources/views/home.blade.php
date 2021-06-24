b82f62072157156b1f624c520c10cc9bd486e56d
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-06-24 15:32:45 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,8 +54,9 @@
             </div>
         </div>
         <!-- Scripts -->
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/materialize.min.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script src="{{ asset('js/particles.min.js') }}"></script>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
