0ce9bff013f65f81933ef829befc546f622aab5b
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-06-27 15:07:43 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
         <!-- Styles -->
         <!-- Compiled and minified CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link rel="stylesheet" href="{{ asset('css/font-awesome/css/font-awesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="url('css/font-awesome/css/font-awesome.min.css')">
+        <link rel="stylesheet" href="url('css/app.css')">
     </head>
     <body>
         <div id="app">
@@ -59,7 +59,7 @@
         <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script src="{{ asset('js/particles.min.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ url('js/particles.min.js') }}"></script>
+        <script src="{{ url('js/app.js') }}"></script>
     </body>
 </html>
