1700dcde657c989d6588a0799821b749e12f6736
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-08-17 21:17:35 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'HomeController@index')->name('index');
+
 Route::post('/affiliate/subscribe', function(){
     $email = request('email');
-    $name = request('name');
 
-    Newsletter::subscribe($email, ['FNAME'=> $name], 'subscribers');
+    Newsletter::subscribe($email, 'subscribers');
     Session::flash('subscribed', 'You have successfully subscribed to our newsletter.');
-    return redirect()->route('home');
+    return redirect()->away('https://mywa.link/jel7e29f');
 });
 
-
-Auth::routes();
-
-Route::get('/access/6-figures', 'HomeController@index')->name('home');
+Route::get('/access', 'HomeController@access')->name('access');
