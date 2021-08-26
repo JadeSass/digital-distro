f79b2d18fa36be83d1137b08d2fa3728becde698
Jade Sass <48350133+JadeSass@users.noreply.github.com>
2021-08-26 17:58:13 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,33 +39,6 @@
         <link rel="stylesheet" href="{{asset('css/styles.min.css')}}">
     </head>
     <body>
-          <style>
-      #timer {
-    font-size: 3em;
-    font-weight: 100;
-    color: white;
-    padding: 20px;
-    width: 100%;
-    color: white;
-
-}
-      #timer > div {
-    display: inline-block;
-    min-width: 90px;
-    padding: 15px;
-    background: #020b43;
-    border-radius: 10px;
-    border: 2px solid #030d52;
-    margin: 15px;
-}
-      #timer > div > span {
-    color: #ffffff;
-    display: block;
-    margin-top: 15px;
-    font-size: .35em;
-    font-weight: 400;
-}
-    </style>
             <div style="background: rgb(244,4,4);padding: 17px;">
                 <p class="text-break text-capitalize d-block rubberBand animated" style="color: rgb(255,191,2);"><strong>proven unusual way to make money online with affiliate marketing&nbsp;</strong></p>
             </div>
@@ -158,30 +131,6 @@ insert_event_code_here;
         <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
         <script src="{{asset('js/script.min.js')}}"></script>
-        <script>
-            future =function updateTimer() {
-    future = Date.parse("aug 29, 2021 01:30:00");
- now = new Date();
- diff = future - now;
-
- days = Math.floor(diff / (1000 * 60 * 60 * 24));
- hours = Math.floor(diff / (1000 * 60 * 60));
- mins = Math.floor(diff / (1000 * 60));
- secs = Math.floor(diff / 1000);
-
- d = days;
- h = hours - days * 24;
- m = mins - hours * 60;
- s = secs - mins * 60;
-
- document.getElementById("timer")
-  .innerHTML =
-  '<div>' + d + '<span>Days</span></div>' +
-  '<div>' + h + '<span>Hours</span></div>' +
-  '<div>' + m + '<span>Minutes</span></div>' +
-  '<div>' + s + '<span>Seconds</span></div>';
-}
-            setInterval('updateTimer()', 1000);
-        </script>
+       
     </body>
 </html>
